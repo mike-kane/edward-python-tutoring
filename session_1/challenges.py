4f071d254722296ee1ee4b7f5655ae5f351d7143
Mike Kane <guido.vanrawesome@gmail.com>
2020-07-24 09:26:26 -0700

    initial commit

---
+++ session_1/challenges.py
@@ -0,0 +1,32 @@
+print("it worked!")
+
+# Here are some practice problems you should be able to puzzle out after Session 1
+
+## Problem  : Create an empty list called "list_1". Append 5 different integers to it
+
+
+## Problem  : Create another empty list called "list_2". Then, write a loop that squares each number in list_1 and appends it to list_2
+
+
+## Problem  : Write an if/else statement to print if a number is odd or even.
+
+
+## Problem  : Use your if/else statement with a for loop to print "odd" or "even" for every item in list_2
+
+
+## Problem  :  Create a dictionary containing key value pairs for your name, email address, and any other pertinent information
+
+
+## Problem  : Print every number between 0 and 100
+
+
+## Problem  : Write a function that prints every number up to a given input
+
+
+## Problem  : Modify your function so that you can choose a starting number, an ending number, and how many numbers to count by (count by 2s, by 3s, etc)
+
+
+## Problem  : Fizzbuzz
+
+
+## Problem  : Rock Paper Scissors
